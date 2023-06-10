88c6ae749e7dbec469dfb80358fa8b3ea21766f0
s1300098 <s1300098@u-aizu.ac.jp>
2023-06-10 15:58:48 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -7,6 +7,11 @@ DiceGame(){
 
 public static void main(String[] args){
     int sum=0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What is your name?");
+    System.out.print("> ");
+String name = sc.next();
+System.out.println("Hello, "+name+"!");
      System.out.println("Rolling dice...");
      Random r = new Random();
      int i=1;
